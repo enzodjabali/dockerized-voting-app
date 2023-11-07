5e7b962458fda901f31065e072769e1b270c10be
Enzo DJABALI <enzo.djabali@supinfo.com>
2023-11-07 16:19:39 +0100

    all env

--- src/worker/Program.cs
+++ src/worker/Program.cs
@@ -12,11 +12,36 @@ namespace Worker
 {
     public class Program
     {
+        public static class DotEnv
+        {
+            public static void Load(string filePath)
+            {
+                if (!File.Exists(filePath))
+                    return;
+
+                foreach (var line in File.ReadAllLines(filePath))
+                {
+                    var parts = line.Split(
+                        '=',
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    if (parts.Length != 2)
+                        continue;
+
+                    Environment.SetEnvironmentVariable(parts[0], parts[1]);
+                }
+            }
+        }
+
         public static int Main(string[] args)
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=postgres;Username=postgres;Password=postgres;");
+                var root = Directory.GetCurrentDirectory();
+                var dotenv = Path.Combine(root, ".env");
+                DotEnv.Load(dotenv);
+
+                var pgsql = OpenDbConnection("Server=postgres;Username=" + Environment.GetEnvironmentVariable("POSTGRES_USER") + ";Password=" + Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") + ";");
                 var redisConn = OpenRedisConnection("redis");
                 var redis = redisConn.GetDatabase();
 
